b0b5aa0bae2597a6ebb30768e4f03c968168b82f
Liadi <liadi.omotola@gmail.com>
2018-01-25 13:20:27 +0100

    exercise 2

--- exercise_2.js
+++ exercise_2.js
@@ -1,3 +1,13 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Colors;
 (function (Colors) {
     Colors[Colors["Green"] = 0] = "Green";
@@ -10,14 +20,37 @@ var colorChange = /** @class */ (function () {
         this.div = div;
     }
     colorChange.prototype.changeColor = function (color) {
+        if (typeof (color) === "number") {
+            return true;
+        }
         this.div.style.backgroundColor = color;
         return true;
     };
     return colorChange;
 }());
+var numericColor = /** @class */ (function (_super) {
+    __extends(numericColor, _super);
+    function numericColor(div) {
+        var _this = _super.call(this, div) || this;
+        _this.div.style.width = squareSize;
+        _this.div.style.height = squareSize;
+        return _this;
+    }
+    numericColor.prototype.changeColor = function (color) {
+        this.div.style.backgroundColor = Colors[color];
+        return true;
+    };
+    numericColor.Colors = Colors;
+    return numericColor;
+}(colorChange));
 var elementSets = [];
 var squareSizeNum = 100;
 var squareSize = squareSizeNum + "px";
+var getRandomIntInclusive = function (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 for (var index = 0; index < 4; index++) {
     elementSets.push({
         'div': document.createElement('div'),
@@ -25,12 +58,12 @@ for (var index = 0; index < 4; index++) {
     });
 }
 elementSets.map(function (elem, index) {
-    var colorChangeClass = new colorChange(elem.div);
+    var colorChangeClass = new numericColor(elem.div);
     elem.div.style.width = squareSize;
     elem.div.style.height = squareSize;
     elem.button.textContent = "Change Color";
     elem.button.onclick = function (event) {
-        colorChangeClass.changeColor(Colors[index]);
+        colorChangeClass.changeColor(getRandomIntInclusive(0, 3));
     };
     document.body.appendChild(elem.button);
     document.body.appendChild(elem.div);
--- exercise_2.ts
+++ exercise_2.ts
@@ -15,15 +15,37 @@ class colorChange {
   constructor(div: Element) {
     this.div = div;
   }
-  changeColor(color: string) : boolean {
+  changeColor(color: string | number) : boolean {
+    if (typeof(color) === "number") {
+      return true;
+    }
     (this.div as HTMLElement).style.backgroundColor = color;
     return true;
   }
 }
 
+class numericColor extends colorChange {
+    static Colors = Colors;
+    constructor(div: Element) {
+        super(div);
+        (this.div as HTMLElement).style.width = squareSize;
+        (this.div as HTMLElement).style.height = squareSize;
+    }
+    changeColor (color: number) : boolean{
+        (this.div as HTMLElement).style.backgroundColor = Colors[color];
+        return true;
+    }
+}
+
 let elementSets: Array<ElementSet> = [];
 let squareSizeNum: number = 100;
 let squareSize: string = `${ squareSizeNum }px`;
+
+let getRandomIntInclusive: Function = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 for (let index: number = 0; index < 4; index ++) {
   elementSets.push({
@@ -33,12 +55,12 @@ for (let index: number = 0; index < 4; index ++) {
 }
 
 elementSets.map( (elem, index) => {
-  let colorChangeClass = new colorChange(elem.div);
+  let colorChangeClass = new numericColor(elem.div);
   (elem.div as HTMLElement).style.width = squareSize;
   (elem.div as HTMLElement).style.height = squareSize;
   elem.button.textContent = "Change Color";
   (elem.button as HTMLElement).onclick = (event) => {
-    colorChangeClass.changeColor(Colors[index]);
+    colorChangeClass.changeColor(getRandomIntInclusive(0, 3));
   }
   document.body.appendChild(elem.button);
   document.body.appendChild(elem.div);
